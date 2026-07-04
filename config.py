4a9cd12c9e45d8e112c344ee0d9f7cc320521e35
Commander004 <artin.golestani1391@gmail.com>
2026-07-04 18:46:07 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-# config.py
 import random
 
-RSS_URL = "https://www.iribnews.ir/fa/rss/allnews"
-          "https://www.isna.ir/rss"
-          "https://www.mehrnews.com/rss"
-          "https://www.farsnews.ir/rss"
-          "https://www.yjc.ir/fa/rss"
+NEWS_SOURCES = [
+    "https://www.iribnews.ir/fa/rss/allnews",
+    "https://www.isna.ir/rss",
+    "https://www.mehrnews.com/rss",
+    "https://www.farsnews.ir/rss",
+    "https://www.yjc.ir/fa/rss"
+]
 
-CHECK_INTERVAL = 30  # هر چند ثانیه یکبار چک کن
 
 BOT_TOKEN = "988843357:oHODR91LgeoclF4vflLzAsKeTPig-qnlCww"
 
