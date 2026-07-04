f928893195a2ce10049eb6c56e4f2aad1d3017dc
Commander004 <artin.golestani1391@gmail.com>
2026-07-04 18:39:32 +0330

    Update config.py

--- config.py
+++ config.py
@@ -12,15 +12,3 @@ CHECK_INTERVAL = 30  # هر چند ثانیه یکبار چک کن
 BOT_TOKEN = "988843357:oHODR91LgeoclF4vflLzAsKeTPig-qnlCww"
 
 CHANNEL_ID = "@AkhbarLahzaei_ir"
-
-
-
-import random
-
-NEWS_SOURCES = [
-    "https://www.iribnews.ir/fa/rss/allnews",
-    "https://www.isna.ir/rss",
-    "https://www.mehrnews.com/rss",
-    "https://www.farsnews.ir/rss",
-    "https://www.yjc.ir/fa/rss"
-]
